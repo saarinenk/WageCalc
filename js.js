b684364cc1b7fd7a20979affe2b746c1b3dcaf3a
Katri Saarinen <katri@dhcp-86-58.eduroam.aalto.fi>
2017-03-07 14:11:33 +0200

    added_files

---
+++ js.js
@@ -0,0 +1,108 @@
+window.onload = function () {
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    fileInput.addEventListener('change', function (e) {
+        var file = fileInput.files[0];
+		var textType = /text.*/;
+            
+        // checks that uploaded file is a text-file
+		if (file.type.match(textType)) {
+            var reader = new FileReader();
+			reader.onload = function (e) {
+                
+                var employees = {};
+                
+                var lines = this.result.split('\n');
+                    
+                for (var line = 1; line < lines.length - 1; line++) {
+                    var personInfo = lines[line].split(",");
+                    console.log(personInfo)
+                    var wage = wage_counter(personInfo[3], personInfo[4]);
+                    if (employees[personInfo[0]] !== undefined) {
+                        console.log(personInfo[0])
+                        employees[personInfo[0]] = (Number(employees[personInfo[0]]) + wage);
+                    } else {
+                        console.log(personInfo[1])
+                        employees[Number(personInfo[1])] = personInfo[0];
+                        console.log(employees[personInfo[1]])
+                        employees[personInfo[0]] = wage;
+                    }
+                };
+                
+                var list = new Array();
+                
+                for (i in employees) {
+                    if (!isNaN(i)) {
+                        list.push(i + ", " + employees[i] + ", $" + employees[employees[i]].toFixed(2));
+                    }
+                }
+
+                
+                fileDisplayArea.innerText = list.join('\n');
+                
+            };
+
+			reader.readAsText(file);
+
+			} else {
+				fileDisplayArea.innerText = "File not supported, it should be a text file!";
+			}
+		});
+}
+
+/* Counts the wage of one day using the start and end times. **/
+
+function wage_counter(s, e) {
+    var wage = 0.0
+    var hours = 0.0
+    var sArray = s.split(":")
+    var eArray = e.split(":")
+    var start_h = Number(sArray[0]) + (sArray[1] / 60.0)
+    var end_h = Number(eArray[0]) + (eArray[1] / 60.0) 
+    
+// counts the wage and work hours of the employee, 
+// taking evening work compensations into account.
+// Handles all needed cases (for example days starting before 6 or ending after midnight).
+    
+    if (start_h < 6) {
+      if (end_h > start_h) {
+          hours = end_h - start_h
+          if (end_h < 6) wage = hours * 3.75 + (end_h - start_h) * 1.15
+          else if (end_h <= 18) wage = hours * 3.75 + (6 - start_h) * 1.15
+          else wage = (hours * 3.75) + (end_h - 18 + 6 - start_h) * 1.15
+      } else {
+        hours = 24 - start_h + end_h
+        wage = hours * 3.75 + (24 - 18 + end_h + 6 - start_h) * 1.15
+      }
+    } else if (start_h < 18) {
+      if (end_h > start_h) {
+          hours = end_h - start_h
+          if (end_h <= 18) wage = hours * 3.75
+          else wage = (hours * 3.75) + (end_h - 18) * 1.15
+      } else {
+        hours = 24 - start_h + end_h
+        wage = hours * 3.75 + (24 - 18 + end_h) * 1.15
+      }
+    } else {
+      if (end_h > start_h) {
+        hours = end_h - start_h
+        wage = hours * (3.75 + 1.15)
+      }
+      else {
+        hours = 24 - start_h + end_h
+        wage = ((24 - start_h) + end_h) * (3.75 + 1.15) 
+      }
+    }
+    
+        // adds overtime compensation to wage
+    
+    if (hours > 8) {
+      if (hours < 10) wage += (hours - 8) * 3.75 * 0.25
+      else if (hours < 12) wage += 2 * 3.75 * 0.25 + (hours - 10) * 3.75 * 0.5
+      else wage += 2 * 3.75 * 0.25 + 2 * 3.75 * 0.5 + (hours - 12) * 3.75
+    }
+    console.log(wage)
+    return wage
+  }
+    
